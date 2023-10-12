b9aba91a943886e57ac697126271af6586f17469
Romeo Van Snick <romeo@romeovansnick.be>
2023-10-12 10:08:55 +0200

    Add documentation

--- app/nts-desktop/page.tsx
+++ app/nts-desktop/page.tsx
@@ -79,6 +79,58 @@ export default async function NTSPage() {
 
 					<DownloadLink />
 				</section>
+
+				<section className={css.installation}>
+					<h2>Usage</h2>
+
+					<h3>Opening NTS Desktop</h3>
+					<p>
+						To open the NTS Desktop pane, click the <code>NTS</code> logo in the menubar.
+					</p>
+
+					<p>
+						You can also press <code>^</code> + <code>N</code>
+					</p>
+
+					<h3>Navigating NTS Desktop</h3>
+					<p>NTS Desktop has three main sections: Channel 1, Channel 2 and Archive Shows.</p>
+					<p>
+						You can navigate between these by pressing the <code>←</code> and <code>→</code> keys, or by clicking the
+						arrows in the NTS Desktop window.
+					</p>
+
+					<h3>Playing the channels</h3>
+					<p>To play a channel, click the black box that says "live now"</p>
+					<p>The menubar icon should reflect the currently playing channel</p>
+
+					<h3>Playing an archived show</h3>
+					<p>To play an archived show, drag the link from your browser onto the NTS logo in the menubar</p>
+					<p>When you open NTS Desktop now, the third panel should display some information about the show</p>
+					<p>You can play, pause or even seek in the show (even backwards seek works!)</p>
+					<p>
+						If you scroll down you can see the tracklist for the show. Clicking any of the items will copy the track
+						info to your paseboard, for easy copying into YouTube or elsewhere.
+					</p>
+
+					<h3>Opening the chat</h3>
+					<p>
+						Clicking the text baloon icon in the Channel 1 or Channel 2 panes will open the chat in your default
+						browser.
+					</p>
+
+					<h3>Opening the live tracklist</h3>
+					<p>
+						Clicking the tracklist icon in the Channel 1 or Channel 2 panes will open the live tracklist in your default
+						browser.
+					</p>
+					<p>
+						Please note that for the live tracklist to work, you need an NTS Friends account and you need to be logged
+						in.
+					</p>
+
+					<h3>Right click menu</h3>
+					<p>You can right-click the NTS icon in the menubar to show a context menu.</p>
+				</section>
 			</div>
 		</>
 	)
