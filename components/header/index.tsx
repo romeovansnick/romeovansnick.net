2ca15eb570f1f70ce1e297598babf1b2c2a3c1e0
Romeo Van Snick <romeo@romeovansnick.be>
2023-10-18 13:21:28 +0200

    Add intro header component

---
+++ components/header/index.tsx
@@ -0,0 +1,16 @@
+import css from "./styles.module.css"
+
+export function Header() {
+	return (
+		<header className={css.header}>
+			<div className={css.hello}>
+				Hi, I'm <h1>Romeo</h1>
+			</div>
+			<div className={css.role}>
+				<em>Software Engineer</em> at <em>Discogs.</em>
+				<br />
+				Based in <em>Amsterdam.</em>
+			</div>
+		</header>
+	)
+}
