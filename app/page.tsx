68809d274288f27dba8751241dfb3e865813837a
Romeo Van Snick <romeo@romeovansnick.be>
2023-11-08 14:49:22 +0100

    Add skills and expertise section

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import { Header } from "~/components/header"
+import { Expertise } from "~/components/expertise"
+import { Skills } from "~/components/skills"
 import { Footer } from "~/components/footer"
 
 import css from "./styles.module.css"
@@ -7,6 +9,10 @@ export default function HomePage() {
 	return (
 		<div className={css.page}>
 			<Header />
+			<div className={css.content}>
+				<Expertise />
+				<Skills />
+			</div>
 			<Footer />
 		</div>
 	)
