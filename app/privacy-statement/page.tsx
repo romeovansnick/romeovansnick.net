74153522e753d1ecc1578b1f7bc4bb7b49a608ba
Romeo Van Snick <romeo@romeovansnick.be>
2023-11-09 16:12:42 +0100

    Add Privacy Statement page

---
+++ app/privacy-statement/page.tsx
@@ -0,0 +1,101 @@
+import css from "./styles.module.css"
+
+export const metadata = {
+	title: "Privacy Statement - Romeo Van Snick",
+}
+
+export default function PrivacyStatementPage() {
+	return (
+		<div className={css.page}>
+			<h1 className={css.title}>Privacy Statement</h1>
+			<p>Romeo Van Snick is responsible for the processing of personal data as shown in this privacy statement.</p>
+
+			<p>
+				<a href="https://romeovansnick.net">https://romeovansnick.net</a> <br />
+				Reinwardtstraat 33A
+				<br />
+				Amsterdam 1093GW, The Netherlands
+				<br />
+				+31202504885
+			</p>
+
+			<p>
+				Romeo Van Snick processes your personal data because you use our services and/or because you provide it to us
+				yourself. Below you will find an overview of the personal data we process:
+				<ul>
+					<li>First and last name,</li>
+					<li>Telephone number</li>
+					<li>Email address</li>
+				</ul>
+			</p>
+
+			<p>
+				Our website and/or service does not intend to collect data about website visitors who are younger than 16 years
+				of age. Unless they have permission from parents or guardians. However, we cannot check whether a visitor is
+				older than 16. We therefore recommend that parents be involved in the online activities of their children, in
+				order to prevent data about children from being collected without parental consent. If you are convinced that we
+				have collected personal information about a minor without this consent, please contact us at
+				contact@romeovansnick.net and we will delete this information.
+			</p>
+			<p>
+				Romeo Van Snick processes your personal data for the following purposes:
+				<ul>
+					<li>Processing your payment,</li>
+					<li>To be able to call or email you if this is necessary to provide our services,</li>
+					<li>To inform you about changes to our services and products,</li>
+					<li>To deliver goods and services, and</li>
+					<li>
+						Romeo Van Snick also processes personal data if we are legally obliged to do so, such as data we need for
+						our tax return.
+					</li>
+				</ul>
+			</p>
+			<p>
+				Romeo Van Snick does not store your personal data for longer than is strictly necessary to achieve the purposes
+				for which your data is collected. We use the following retention periods for the following (categories) of
+				personal data
+			</p>
+			<p>
+				Romeo Van Snick only provides data to third parties and only if this is necessary for the execution of our
+				agreement with you or to comply with a legal obligation.
+			</p>
+			<p>
+				Cookies or similar techniques that we use Romeo Van Snick does not use cookies or similar techniques on this
+				website.
+			</p>
+			<p>
+				You have the right to view, correct or delete your personal data. You can do this yourself via the personal
+				settings of your account. In addition, you have the right to withdraw your consent to data processing or to
+				object to the processing of your personal data by our company and you have the right to data portability. This
+				means that you can submit a request to us to send the personal data we have about you in a computer file to you
+				or another organization mentioned by you.
+			</p>
+			<p>
+				If you would like to exercise your right to object and/or right to data portability or if you have other
+				questions/comments about data processing, please send a specified request to{" "}
+				<a href="mailto:contact@romeovansnick.net">contact@romeovansnick.net</a>.
+			</p>
+			<p>
+				To ensure that the request for access has been made by you, we ask you to enclose a copy of your proof of
+				identity with the request. Make your passport photo, MRZ (machine readable zone, the strip with numbers at the
+				bottom of the passport), passport number and Citizen Service Number (BSN) black in this copy. This is to protect
+				your privacy. Romeo Van Snick will respond to your request as quickly as possible, but in any case within four
+				weeks.
+			</p>
+			<p>
+				Romeo Van Snick would also like to point out that you have the option to file a complaint with the national
+				supervisory authority, the Dutch Data Protection Authority. This can be done via{" "}
+				<a href="https://autoriteitpersoonsgegevens.nl/nl/contact-met-de-autoriteit-persoonsgegevens/tip-ons">
+					this link
+				</a>
+				.
+			</p>
+			<p>
+				How we protect personal data Romeo Van Snick takes the protection of your data seriously and takes appropriate
+				measures to prevent misuse, loss, unauthorized access, unwanted disclosure and unauthorized modification. If you
+				have the impression that your data is not properly secured or there are indications of misuse, please contact
+				our customer service or via <a href="mailto:contact@romeovansnick.net">contact@romeovansnick.net</a>.
+			</p>
+		</div>
+	)
+}
